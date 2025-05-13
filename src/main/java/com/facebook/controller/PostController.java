f4e1393727c048436c8c8905048c7c2822c39457
Sairku <kukrias@gmail.com>
2025-05-13 21:21:36 +0300

    add post2

--- src/main/java/com/facebook/controller/PostController.java
+++ src/main/java/com/facebook/controller/PostController.java
@@ -5,6 +5,7 @@ import com.facebook.dto.PostCreateRequest;
 import com.facebook.dto.PostResponse;
 import com.facebook.dto.PostUpdateRequest;
 import com.facebook.service.PostService;
+import com.facebook.util.ResponseHandler;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,21 +20,22 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/create")
-    public ResponseEntity<PostResponse> createPost(@RequestBody @Valid PostCreateRequest request) {
+    public ResponseEntity<?> createPost(@RequestBody @Valid PostCreateRequest request) {
         PostResponse response = postService.createPost(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseHandler.generateResponse(HttpStatus.CREATED, false, "Post was created", response);
+
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PostResponse> updatePost(
+    public ResponseEntity<?> updatePost(
             @PathVariable Long id,
             @RequestBody @Valid PostUpdateRequest request
     ) {
         if (!id.equals(request.getId())) {
-            return ResponseEntity.badRequest().build();
+            return ResponseHandler.generateResponse(HttpStatus.BAD_REQUEST, true, "Invalid request", null);
         }
 
         PostResponse response = postService.updatePost(request);
-        return ResponseEntity.ok(response);
+        return ResponseHandler.generateResponse(HttpStatus.OK, false, "Post was updated", response);
     }
 }
