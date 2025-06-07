903c719acbc135352204bebf3ead0ff55aec0f2d
Hanna Sachok <hanna.sachok@gmail.com>
2025-06-07 02:38:48 -0500

    Initializing

--- src/main/java/com/facebook/dto/PasswordResetRequest.java
+++ src/main/java/com/facebook/dto/PasswordResetRequest.java
@@ -1,2 +1,12 @@
-package com.facebook.dto;public class PasswordResetRequest {
+package com.facebook.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PasswordResetRequest {
+    @NotBlank(message = "Email required")
+    @Email(message = "Invalid email format")
+    private String email;
 }
--- src/main/java/com/facebook/service/EmailService.java
+++ src/main/java/com/facebook/service/EmailService.java
@@ -1,2 +1,26 @@
-package com.facebook.service;public class EmailService {
+package com.facebook.service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailSender;
+    private final Dotenv dotenv;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        String from = dotenv.get("EMAIL_SENDER_USERNAME", System.getenv("EMAIL_SENDER_USERNAME"));
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        emailSender.send(message);
+    }
 }
